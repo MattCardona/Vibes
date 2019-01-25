248d3cc153b9566c4c9116cceca8f8eb995552cc
Matt <boblikesbbq@gmail.com>
2019-01-24 16:34:36 -0800

    Setup mongoose

--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGO_DB, {useNewUrlParser: true});
+
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.get("/", (req,res) => {
   res.send("This is the home route");
