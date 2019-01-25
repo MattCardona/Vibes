ab2237a96c5f68f0b2e0cfe6026e74be7ad543bc
Matt <boblikesbbq@gmail.com>
2019-01-24 17:02:44 -0800

    Add User model update mongoose version

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ const cookieParser = require('cookie-parser');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGO_DB, {useNewUrlParser: true});
+const { User } = require('./models/user');
+
 
 const port = process.env.PORT || 3000;
 
@@ -17,6 +19,16 @@ app.use(cookieParser());
 app.get("/", (req,res) => {
   res.send("This is the home route");
 })
+
+
+//======================
+// User routes
+//======================
+
+app.post('/api/users/register', (req, res) => {
+  res.status(200).send("You hit the end point")
+})
+
 
 app.listen(port, () => {
   console.log(`The Vibes server is running on port ${port}`);
