4bfff653527803df2f05048e0ad06ce29823d9e7
Matt <boblikesbbq@gmail.com>
2019-01-25 17:04:44 -0800

    Hash Users password

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const SALT = 10;
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -36,6 +38,27 @@ const userSchema = new mongoose.Schema({
   },
   token: {
     type: String
+  }
+});
+
+userSchema.pre('save', function(next){
+  const user = this;
+
+  if(user.isModified('password')){
+    bcrypt.genSalt(SALT, function(err, salt){
+      if(err){
+        return next(err);
+      }
+      bcrypt.hash(user.password, salt, function(err, hash){
+        if(err){
+          return next(err);
+        }
+        user.password = hash;
+        next();
+      });
+    });
+  }else{
+    next();
   }
 });
 
