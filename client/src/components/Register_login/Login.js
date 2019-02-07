8d64a16196901f0a34c2159d0be12b2ce7d48402
Matt <boblikesbbq@gmail.com>
2019-02-06 16:52:56 -0800

    Add Login component

---
+++ client/src/components/Register_login/Login.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import FormField from '../utils/Forms/FormField';
+
+class Login extends Component {
+  constructor(props){
+    super(props);
+    this.submitForm = this.submitForm.bind(this);    
+    this.updateForm = this.updateForm.bind(this);    
+    this.state = {
+      formError: false,
+      formSuccess: '',
+      formdata: {
+        email: {
+          element: 'input',
+          value: '',
+          config: {
+            name: 'email_input',
+            type: 'email',
+            placeholder: 'Enter your email'
+          },
+          validation: {
+            required: true,
+            email: true
+          },
+          valid: false,
+          touched: false,
+          validationMessage: ''
+        },
+        password: {
+          element: 'input',
+          value: '',
+          config: {
+            name: 'password_input',
+            type: 'password',
+            placeholder: 'Enter your password'
+          },
+          validation: {
+            required: true
+          },
+          valid: false,
+          touched: false,
+          validationMessage: ''
+        }
+      }
+    }
+  }
+  submitForm(e) {
+
+  }
+  updateForm(element) {
+
+  }
+  render() {
+    return (
+      <div className="signin_wrapper">
+        <form onSubmit={(event) => this.submitForm(event)}>
+
+          <FormField 
+            id={'email'}
+            formdata={this.state.formdata.email}
+            change={(element) => this.updateForm(element)}
+          />
+
+        </form>
+      </div>
+    )
+  }
+};
+
+export default connect()(Login);
--- client/src/components/Register_login/index.js
+++ client/src/components/Register_login/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MyButton from '../utils/button';
+import Login from './Login';
 
 const RegisterLogin = () => {
   return (
@@ -22,7 +23,7 @@ const RegisterLogin = () => {
         <div className="right">
             <h2>Registered customers</h2>
             <p>If you have an account please login</p>
-            LOGIN
+            <Login />
         </div>
 
       </div>
