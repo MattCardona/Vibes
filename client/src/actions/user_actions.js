844dbcd80fcabac145ae8e592d240c3cf534d989
Matt <boblikesbbq@gmail.com>
2020-07-05 15:02:02 -0700

    Create loginUser action

--- client/src/actions/user_actions.js
+++ client/src/actions/user_actions.js
@@ -1,3 +1,20 @@
 import axios from 'axios';
 
 import { USER_SERVER } from '../components/utils/misc';
+import { LOGIN_USER } from './types';
+import Axios from 'axios';
+
+
+export const loginUser = dataToSubmit => async dispatch => {
+  try {
+    const { data } = await Axios.post(`${USER_SERVER}/login`, dataToSubmit);
+    // console.log(data);
+    dispatch({
+      type: LOGIN_USER,
+      payload: data
+    })
+    return data
+  } catch (error) {
+    console.log(error);
+  }
+}
