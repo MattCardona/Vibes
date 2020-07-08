1e921557da4a64a63a2ff8ac9d20cc414d72573a
Matt <boblikesbbq@gmail.com>
2020-07-08 16:26:29 -0700

    Create /register route

--- client/src/routes.js
+++ client/src/routes.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Layout from './hoc/Layout';
 import RegisterLogin from './components/Register_login';
+import Register from './components/Register_login/Register';
 
 const Routes = () => {
   return (
@@ -11,6 +12,7 @@ const Routes = () => {
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/register_login" component={RegisterLogin} exact />
+        <Route path="/register" component={Register} exact />
       </Switch>
     </Layout>
   )
